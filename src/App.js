c473f9dc61b6fd74e741b1d9e1a887fb012cfef3
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-09-22 22:38:39 -0400

    nav components completed, working on styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from "react";
-import "./App.css";
-
+//router
+import { Switch, Route, Link } from "react-router-dom";
+//components
+import Game from "./pages/Game.js";
+import HighScores from "./pages/HighScores.js";
+import GameOver from "./pages/GameOver.js";
+import Home from "./pages/Home.js";
+import NavBar from "./components/NavBar.js";
+//styles
+import Container from "./styled/Container.js";
+import Main from "./styled/Main.js";
+import GlobalStyles from "./styled/Global.js";
 function App() {
   return (
-    <div>
-      <h1>test</h1>
-    </div>
+    <>
+      <Main>
+        <GlobalStyles />
+        <Container>
+          <NavBar />
+          <Switch>
+            <Route path="/game" component={Game} />
+            <Route path="/highScores" component={HighScores} />
+            <Route path="/gameOver" component={GameOver} />
+            <Route path="/" component={Home} />
+          </Switch>
+        </Container>
+      </Main>
+    </>
   );
 }
 
---
+++ src/styled/Global.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+    color: #333;
+    margin: 0;
+    font-family: sans-serif;
+    font-weight: 300;
+}
+h1, h2 {
+    margin-bottom: 2rem;
+}
+`;
+
+export default GlobalStyle;
