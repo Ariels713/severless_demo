eb3e9ff5e582ff0e225c3f672844a9095da4d0b7
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-09-23 22:40:50 -0400

    score logic complete

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ScoreProvider } from "./contexts/ScoreContext.js";
 
 ReactDOM.render(
   <Router>
-    <App />
+    <ScoreProvider>
+      <App />
+    </ScoreProvider>
   </Router>,
   document.getElementById("root")
 );
--- src/pages/GameOver.js
+++ src/pages/GameOver.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { useScore } from "../contexts/ScoreContext.js";
+import { StyledLink } from "../styled/NavBarStyles.js";
 
-const GameOver = () => {
+const GameOver = ({ history }) => {
+  const [score] = useScore();
+
+  if (score === -1) {
+    history.push("/");
+  }
   return (
     <div>
       <h1>GameOver</h1>
+      <p>{score}</p>
+      <StyledLink to="/">Go Home</StyledLink>
+      <StyledLink to="/game">Play Again</StyledLink>
     </div>
   );
 };
