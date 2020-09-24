ae57a0ae08e7f28a1bd5f98c48cc4562294e2382
Ariel_Rodriguez <32606708+Ariels713@users.noreply.github.com>
2020-09-24 13:51:46 -0400

    adding enviornment variables

--- functions/getHighScores.js
+++ functions/getHighScores.js
@@ -1,17 +1,28 @@
+require("dotenv").config();
 const Airtable = require("airtable");
 
 Airtable.configure({
-  apiKey: "keyAy8mTUaFqRq5q3",
+  apiKey: process.env.AIRTABLE_API_KEY,
 });
 
-const base = Airtable.base("applKfKLlhJfRF2Bz");
-const table = base.table("guest");
+const base = Airtable.base(process.env.AIRTABLE_BASE);
+const table = base.table(process.env.AIRTABLE_TABLE);
 
 exports.handler = async (event) => {
-  const records = await table.select({}).firstPage();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(records),
-  };
+  try {
+    const records = await table.select({}).firstPage();
+    const filteredRecords = records.map((record) => ({
+      id: record.id,
+      fields: record.fields,
+    }));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(filteredRecords),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ err: "Query Failed" }),
+    };
+  }
 };
